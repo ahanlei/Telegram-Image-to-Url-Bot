e8d1dd9ed53e42347ef9fe0106c21e8b1af77730
MastermindVrtx <mastermindvrtx@gmail.com>
2021-05-17 02:09:17 +0845

    Update uploader.py

--- ignite/uploader.py
+++ ignite/uploader.py
@@ -36,5 +36,37 @@ async def getimage(client, message):
                                             InlineKeyboardButton(text="Group Link", url=f"https://t.me/vrtxmusic")
                                             ]])
     )
+
+    
+@Client.on_message(filters.document)
+async def getimage(client, message):
+    tmp = os.path.join("_Hacked_", str(message.chat.id))
+    if not os.path.isdir(tmp):
+        os.makedirs(tmp)
+    img_path = os.path.join(tmp, str(uuid.uuid4()) + ".jpg")
+    dwn = await message.reply_text("Uploading to my server and analyzing...", True)
+    img_path = await client.download_media(message=message, file_name=img_path)
+    await dwn.edit_text("ETA: > sec[░░░░░░              ]")
+    await dwn.edit_text("ETA: > sec[░░░░░░░░░░░░        ]")
+    await dwn.edit_text("ETA: > sec[░░░░░░░░░░░░░░░░░░░░]")
+    await dwn.edit_text("Sending...")
+    
+    
+
+    try:
+        response = upload_file(img_path)
+    except Exception as e:
+        await dwn.edit_text(f"Oops something went wrong\n{error}")
+        return
+    await dwn.edit_text(
+        text=f"<b>Link :-</b> <code>https://telegra.ph{response[0]}</code>",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Open Link", url=f"https://telegra.ph{response[0]}"),
+                                            InlineKeyboardButton(text="Share Link", url=f"https://telegram.me/share/url?url=https://telegra.ph{response[0]}"),
+                                            InlineKeyboardButton(text="Group Link", url=f"https://t.me/vrtxmusic")
+                                            ]])
+    )    
+    
+    
     shutil.rmtree(tmp, ignore_errors=True)
     
